4c97061a2dc5fea328ae168c09dc8fa3174f71c4
叶枫 <645381995@qq.com>
2023-07-11 13:04:09 +0800

    feat: init

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,31 @@
-import { View, Text } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
-import "./index.scss";
+import { useState } from "react";
+import { View, PickerView, PickerViewColumn } from "@tarojs/components";
 
 export default function Index() {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
-
+  const [months, setMonths] = useState(["1", "2"]);
   return (
-    <View className="index">
-      <Text>Hello world!</Text>
+    <View>
+      <PickerView
+        indicatorStyle="height: 50px;"
+        style="width: 100%; height: 300px;"
+        onChange={(event) => {
+          const { value } = event.detail;
+          if (value[0] === 1) {
+            setMonths(["1", "2", "3", "4", "5"]);
+          }
+        }}
+      >
+        <PickerViewColumn>
+          {["2022", "2022"].map((item) => (
+            <View key={item}>{item}年</View>
+          ))}
+        </PickerViewColumn>
+        <PickerViewColumn>
+          {months.map((item) => (
+            <View key={item}>{item}月</View>
+          ))}
+        </PickerViewColumn>
+      </PickerView>
     </View>
   );
 }
